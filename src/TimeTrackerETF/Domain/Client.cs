90af9dcf6ee0f6740a614f124dfea02fd5a17843
Zoran Pantos <pantos78zoran@gmail.com>
2019-07-11 17:03:48 +0200

    Add domain models

---
+++ src/TimeTrackerETF/Domain/Client.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TimeTrackerETF.Domain
+{
+    public class Client
+    {
+        public long Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+    }
+}
---
+++ src/TimeTrackerETF/Domain/Project.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace TimeTrackerETF.Domain
+{
+    public class Project
+    {
+        public long Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public Client Client { get; set; }
+    }
+}
